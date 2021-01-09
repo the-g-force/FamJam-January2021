02f3765acde8d3415d22d61dee817c2aeded0bf3
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-09 11:05:09 -0500

    Stub in story scene

--- project/src/MenuScene.gd
+++ project/src/MenuScene.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://src/Level.tscn")
+
+
+func _on_StoryButton_pressed():
+	get_tree().change_scene("res://src/StoryScene.tscn")
