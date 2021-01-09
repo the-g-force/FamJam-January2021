69e53ac82c8fdb8bf26da027c391dc435d123f2d
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-09 13:23:49 -0500

    Follow naming conventions for _destroyed

--- project/src/Alien.gd
+++ project/src/Alien.gd
@@ -5,7 +5,7 @@ signal destroyed(location)
 
 export var speed := 100
 export var shoot_delay := 2
-var destroyed := false
+var _destroyed := false
 
 onready var _AlienBullet := load("res://src/AlienBullet.tscn")
 
@@ -17,20 +17,20 @@ func _ready():
 
 
 func _process(delta):
-	var yspeed := 0 if not destroyed else 500*delta
-	var xspeed = -(speed*delta) if not destroyed else (-300*delta)
+	var yspeed := 0 if not _destroyed else 500*delta
+	var xspeed = -(speed*delta) if not _destroyed else (-300*delta)
 	
 	var velocity := Vector2(xspeed, yspeed)
 	var collision := move_and_collide(velocity)
-	if position.y >= get_viewport_rect().size.y-40 and destroyed:
+	if position.y >= get_viewport_rect().size.y-40 and _destroyed:
 		var _explosion:Node2D = load("res://src/Explosion.tscn").instance()
 		_explosion.position = get_global_transform().origin
 		get_parent().add_child(_explosion)
 		queue_free()
-	if collision!=null and not destroyed:
+	if collision!=null and not _destroyed:
 		damage()
 	
-	if destroyed:
+	if _destroyed:
 		var wobble := randi()%2
 		if wobble == 0:
 			$Sprite.rotation_degrees += 5
@@ -50,5 +50,5 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 func damage():
 	emit_signal("destroyed", self)
-	destroyed = true
+	_destroyed = true
 	$Timer.stop()
