f6976abaca9892700999f426eb8b69f0e504570e
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-09 13:29:28 -0500

    Remove compiler warnings

--- project/src/DeathControl.gd
+++ project/src/DeathControl.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _on_MenuButton_pressed():
-	get_tree().change_scene("res://src/MenuScene.tscn")
+	var _ignored := get_tree().change_scene("res://src/MenuScene.tscn")
 
 
 func _on_PlayAgainButton_pressed():
-	get_tree().change_scene("res://src/Level.tscn")
+	var _ignored := get_tree().change_scene("res://src/Level.tscn")
