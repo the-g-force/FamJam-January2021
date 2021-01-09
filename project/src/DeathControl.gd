5f6c9be6c079b19e59c8c0027d598f7a0e868511
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-09 17:11:02 -0500

    Remove redundant setting of duck_is_dead
    It can just be done in the ready method of Level.

--- project/src/DeathControl.gd
+++ project/src/DeathControl.gd
@@ -6,11 +6,9 @@ func play_animation()->void:
 
 func _on_MenuButton_pressed():
 	Global.play_button_sound()
-	Global.duck_is_dead = false
 	var _ignored := get_tree().change_scene("res://src/MenuScene.tscn")
 
 
 func _on_PlayAgainButton_pressed():
 	Global.play_button_sound()
-	Global.duck_is_dead = false
 	var _ignored := get_tree().change_scene("res://src/Level.tscn")
