9c3b547fae024909da51673526406c622beabba9
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-09 13:18:34 -0500

    Add fullscreen toggle

--- project/src/MenuScene.gd
+++ project/src/MenuScene.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+onready var _fullscreen_button := $FullScreenButton
+
+func _ready():
+	_fullscreen_button.pressed = OS.window_fullscreen
+
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://src/Level.tscn")
@@ -8,3 +13,7 @@ func _on_PlayButton_pressed():
 
 func _on_StoryButton_pressed():
 	get_tree().change_scene("res://src/StoryScene.tscn")
+
+
+func _on_FullScreenButton_pressed():
+	OS.window_fullscreen = _fullscreen_button.pressed
